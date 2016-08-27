bbfcb62703aa751f29bfc80d4518c32adb0010c1
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-27 15:38:22 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -1,20 +1,26 @@
 function SomeDirective() {
-	return {
-		template: [
-			'<div>',
-				'<h3>{{ counter }}</h3>',
-			'</div>'
-		].join(''),
-		controller: function($scope) {
-			$scope.counter = 0;
-		},
-		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
-
-		}
-	}
+  return {
+    template: [
+      '<div>',
+      '<h3>{{ some.counter }}</h3>',
+      '</div>'
+    ].join(''),
+    controller: function($scope) {
+      // $scope.counter = 0;
+      this.counter = 0;
+    },
+    controllerAs: 'some',
+    // require: 'someDirective',
+    link: function (scope, elem, attrs, ctrl) {
+      // var actualElem = elem[0];
+      document.addEventListener('keydown', function(event) {
+	ctrl.counter += 1;
+	scope.$digest();
+      });
+    }
+  }
 }
 
 angular
-	.module('app')
-	.directive('someDirective', SomeDirective);
+  .module('app')
+  .directive('someDirective', SomeDirective);
